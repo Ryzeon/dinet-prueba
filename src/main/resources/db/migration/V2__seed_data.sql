d3155f13c02b788edb75399014b21d25170c6614
Alex Avila <dev@ryzeon.me>
2026-03-27 23:26:13 -0400

    feat(db): created seed for zones and clientes.

---
+++ src/main/resources/db/migration/V2__seed_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO zonas (id, soporte_refrigeracion) VALUES
+    ('ZONA1', TRUE),
+    ('ZONA2', FALSE),
+    ('ZONA3', TRUE),
+    ('ZONA5', FALSE);
+
+INSERT INTO clientes (id, activo) VALUES
+    ('CLI-123', TRUE),
+    ('CLI-999', TRUE),

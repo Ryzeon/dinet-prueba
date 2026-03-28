069b335bccc6fdc3d900c2a8b2ca3e0cb186e072
Alex Avila <dev@ryzeon.me>
2026-03-27 22:05:05 -0400

    chore: initialize project structure with maven dependencies

---
+++ src/main/java/me/ryzeon/dinet/DinetPruebaApplication.java
@@ -0,0 +1,13 @@
+package me.ryzeon.dinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinetPruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinetPruebaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ryzeon/dinet/DinetPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ryzeon.dinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinetPruebaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c9f4a81675f2da1b4153468faa13cdd2538c21fa
Alex Avila <dev@ryzeon.me>
2026-03-28 01:19:53 -0400

    feat(config): rename pedidos carga to orders import in application.yaml

--- src/main/java/me/ryzeon/dinet/DinetPruebaApplication.java
+++ src/main/java/me/ryzeon/dinet/DinetPruebaApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+/**
+ * <p>Created by Alex Avila Asto - A.K.A (Ryzeon)</p>
+ * <p>Project: dinet-prueba</p>
+ * <p>Date: 28/03/26 @ 00:48</p>
+ */
+@SpringBootApplication(scanBasePackages = "me.ryzeon.dinet")
 @ConfigurationPropertiesScan
 public class DinetPruebaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DinetPruebaApplication.class, args);
     }
-
 }
--- src/test/java/me/ryzeon/dinet/DinetPruebaApplicationTests.java
+++ src/test/java/me/ryzeon/dinet/DinetPruebaApplicationTests.java
@@ -3,11 +3,15 @@ package me.ryzeon.dinet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * <p>Created by Alex Avila Asto - A.K.A (Ryzeon)</p>
+ * <p>Project: dinet-prueba</p>
+ * <p>Date: 27/03/26 @ 22:05</p>
+ */
 @SpringBootTest
 class DinetPruebaApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }

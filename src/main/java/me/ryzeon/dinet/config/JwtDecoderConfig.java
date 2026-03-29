970cd1e70d353081b81f4c5078a26f2f7d7c912a
Alex Avila <dev@ryzeon.me>
2026-03-28 23:38:25 -0400

    feat: added jwt decoder.

---
+++ src/main/java/me/ryzeon/dinet/config/JwtDecoderConfig.java
@@ -0,0 +1,28 @@
+package me.ryzeon.dinet.config;
+
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+/**
+ * <p>Created by Alex Avila Asto - A.K.A (Ryzeon)</p>
+ * <p>Project: dinet-prueba</p>
+ * <p>Date: 28/03/26 @ 16:46</p>
+ */
+@Configuration
+public class JwtDecoderConfig {
+
+    @Bean
+    public JwtDecoder jwtDecoder(DinetSecurityProperties properties) {
+        byte[] keyBytes = properties.secret().getBytes(StandardCharsets.UTF_8);
+        SecretKey secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKey)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+}

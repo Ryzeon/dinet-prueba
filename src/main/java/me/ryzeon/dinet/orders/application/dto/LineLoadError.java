e7623e4799247e417def026950c0e0dc8502b16d
Alex Avila <dev@ryzeon.me>
2026-03-30 00:01:13 -0400

    feat: add JSON property annotations for LineLoadError and OrderLoadErrorGroup

--- src/main/java/me/ryzeon/dinet/orders/application/dto/LineLoadError.java
+++ src/main/java/me/ryzeon/dinet/orders/application/dto/LineLoadError.java
@@ -1,5 +1,6 @@
 package me.ryzeon.dinet.orders.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import me.ryzeon.dinet.orders.domain.validation.OrderLoadErrorCode;
 
 /**
@@ -7,7 +8,10 @@ import me.ryzeon.dinet.orders.domain.validation.OrderLoadErrorCode;
  * <p>Project: dinet-prueba</p>
  * <p>Date: 28/03/26 @ 01:28</p>
  */
-public record LineLoadError(int lineNumber, OrderLoadErrorCode code, String detail) {
+public record LineLoadError(
+        @JsonProperty("numeroLinea") int lineNumber,
+        @JsonProperty("codigo") OrderLoadErrorCode code,
+        @JsonProperty("detalle") String detail) {
 
     public LineLoadError {
         detail = detail == null ? "" : detail;
--- src/main/java/me/ryzeon/dinet/orders/application/dto/OrderLoadErrorGroup.java
+++ src/main/java/me/ryzeon/dinet/orders/application/dto/OrderLoadErrorGroup.java
@@ -1,11 +1,14 @@
 package me.ryzeon.dinet.orders.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * <p>Created by Alex Avila Asto - A.K.A (Ryzeon)</p>
  * <p>Project: dinet-prueba</p>
  * <p>Date: 28/03/26 @ 01:28</p>
  */
-public record OrderLoadErrorGroup(String contractCode, long count) {
+public record OrderLoadErrorGroup(
+        @JsonProperty("codigo") String contractCode, @JsonProperty("cantidad") long count) {
 
     public OrderLoadErrorGroup {
         if (count < 0) {
